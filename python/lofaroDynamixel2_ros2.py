4a61f6e1bf6e4fc69c8ff1f05a98a3bb57b7b2e6
Daniel M. Lofaro <dan@danlofaro.com>
2023-02-28 17:14:08 -0500

    getting an error about a non type in the setupt rutine

--- python/lofaroDynamixel2_ros2.py
+++ python/lofaroDynamixel2_ros2.py
@@ -9,29 +9,44 @@ node  = None
 pub   = None
 sub   = None
 
-ENUM_NAME    = 0
-ENUM_REF     = 1
-ENUM_STATE   = 2
-ENUM_ENABLED = 3
+ENUM_NAME          = 0
+ENUM_REF           = 1
+ENUM_STATE         = 2
+ENUM_ENABLED       = 3
+ENUM_POS_MAX       = 4
+ENUM_POS_MIN       = 5
+ENUM_MOT_INDEX     = 6
 
-#        name   pos_id  enc_id  enabled
-IDs = { ("rhy", 0x1c,   0x10,   False ),
-        ("rhr", 0x1d,   0x12,   False  ),
-        ("rhp", 0x1e,   0x14,   False  ),
-        ("rkn", 0x1f,   0x16,   False  ),
-        ("ray", 0x18,   0x18,   False  ),
-        ("rar", 0x19,   0x19,   False  ),
-        ("rap", 0x1a,   0x1a,   False  ),
-        ("rtp", 0x1b,   0x1b,   True  ), 
-        ("lhy", 0x3c,   0x30,   False ),
-        ("lhr", 0x3d,   0x32,   False ),
-        ("lhp", 0x3e,   0x34,   False ),
-        ("lkn", 0x3f,   0x36,   False ),
-        ("lay", 0x38,   0x38,   False ),
-        ("lar", 0x39,   0x39,   False ),
-        ("lap", 0x3a,   0x3a,   False ),
-        ("ltp", 0x3b,   0x3b,   False ) 
+the_id_i = -1
+def getNextI():
+  global the_id_i
+  the_id_i += 1
+  return the_id_i
+
+#        name   pos_id  enc_id  enabled  max_pos (rad)   min_pos (rad)  filter_id
+IDs = { ("rhy", 0x1c,   0x10,   False,   1.0,            -1.0,          getNextI()),
+        ("rhr", 0x1d,   0x12,   False,   0.2,            -0.2,          getNextI()),
+        ("rhp", 0x1e,   0x14,   False,   0.7,            -1.3,          getNextI()),
+        ("rkn", 0x1f,   0x16,   False,   1.2,             0.0,          getNextI()),
+        ("ray", 0x18,   0x18,   False,   1.0,            -1.0,          getNextI()),
+        ("rar", 0x19,   0x19,   False,   1.0,            -1.0,          getNextI()),
+        ("rap", 0x1a,   0x1a,   False,   0.4,            -1.0,          getNextI()),
+        ("rtp", 0x1b,   0x1b,   True ,   1.0,            -1.0,          getNextI()), 
+        ("lhy", 0x3c,   0x30,   False,   1.0,            -1.0,          getNextI()),
+        ("lhr", 0x3d,   0x32,   False,   0.2,            -0.2,          getNextI()),
+        ("lhp", 0x3e,   0x34,   False,   0.7,            -1.3,          getNextI()),
+        ("lkn", 0x3f,   0x36,   False,   1.2,             0.0,          getNextI()),
+        ("lay", 0x38,   0x38,   False,   1.0,            -1.0,          getNextI()),
+        ("lar", 0x39,   0x39,   False,   1.0,            -1.0,          getNextI()),
+        ("lap", 0x3a,   0x3a,   False,   0.4,            -1.0,          getNextI()),
+        ("ltp", 0x3b,   0x3b,   False,   1.0,            -1.0,          getNextI()) 
       }
+
+FILTER_REF_0    = None
+FILTER_REF_1    = None
+FILTER_L        = None
+FILTER_MOT_NUM  = len(IDs)
+STATE_POS       = None
 
 def callback(msg):
   m = msg
@@ -51,8 +66,23 @@ def callback(msg):
 #      pass
   pass
 
-def init():
-  global robot, node, pub, sub
+def init(L=None):
+  global robot, node, pub, sub, FILTER_REF_0, FILTER_REF_1, FILTER_L
+  
+  if (L==None):
+    L = 100
+  if (L < 0):
+    L = 100
+  
+  FILTER_L = L
+
+  FILTER_REF_0 = [0.0]  
+  FILTER_REF_1 = [0.0]
+  STATE_POS    = [0.0]
+  for i in range(1, FILTER_MOT_NUM):
+    FILTER_REF_0.append(0.0)
+    FILTER_REF_1.append(0.0)
+    STATE_POS.append(0.0)
 
   robot = ld2.LofaroDynamixel2(baud=3000000, port='/dev/ttyUSB0')
   robot.open()
@@ -61,6 +91,26 @@ def init():
   node = rclpy.create_node('lofaro_dynamixel2_ros2_node')
   pub  = node.create_publisher(JointState, '/state',1)
   sub  = node.create_subscription(JointState,'/ref', callback, 10)
+
+  for i in range(10):
+    init_filter_ref()
+    time.sleep(0.05)
+
+def init_filter_ref():
+  global FILTER_REF_0, FILTER_REF_1, STATE_POS
+  for p in IDs:
+    if p[ENUM_ENABLED]:
+      name        = p[ENUM_NAME]
+      the_id      = p[ENUM_STATE]
+      mot_index   = p[ENUM_MOT_INDEX]
+      print(mot_index)
+      pos, err    = robot.getPos(the_id)
+      print(pos)
+      if err == robot.OK:
+          STATE_POS[mot_index] = pos
+          FILTER_REF_0[mot_index] = pos
+          FILTER_REF_1[mot_index] = pos
+
 
 def torqueEnable():
   for p in IDs:
@@ -95,15 +145,18 @@ def putPos():
   pass
 
 def getPos():
+  global STATE_POS
   state = JointState()
   for p in IDs:
     if p[ENUM_ENABLED]:
-      name   = p[ENUM_NAME]
-      the_id = p[ENUM_STATE]
+      name        = p[ENUM_NAME]
+      the_id      = p[ENUM_STATE]
+      mot_index   = P[ENUM_MOT_INDEX]
       pos, err    = robot.getPos(the_id)
       if err == robot.OK:
         state.name.append(name)
         state.position.append(pos)
+        STATE_POS[mot_index] = pos
 
   pub.publish(state) 
   pass
