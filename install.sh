1f426662bb51eefaca3cca5d59a21298b38b093e
Daniel M. Lofaro <dan@danlofaro.com>
2023-05-19 23:31:06 -0400

    switched names of the repo and adding a sophia h file

---
+++ install.sh
@@ -0,0 +1,4 @@
+INSTALL_DIR=/etc/sophia/
+rm -rf $INSTALL_DIR
+mkdir -p $INSTALL_DIR
+cp -r python $INSTALL_DIR
---
+++ python/sophia_h.py
@@ -0,0 +1,5 @@
+class Sophia(self, args=None):
+  self.ROS_CHAN_REF_POS='/ref/pos'
+  self.ROS_CHAN_STATE_POS='/state/pos'
+  self.ROS_CHAN_STATE_TORQUE='/state/torque'
+
