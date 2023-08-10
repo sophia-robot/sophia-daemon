1ba09e618530fb4809987d3acbb6837dee438f1c
Daniel M. Lofaro <dan@danlofaro.com>
2023-08-09 23:15:17 -0400

    worked for turning off the legs

--- python/sophia_h.py
+++ python/sophia_h.py
@@ -13,8 +13,8 @@ class Sophia:
 
   NAME = 0
 
-  ENABLE_RIGHT = True
-  ENABLE_LEFT  = True
+  ENABLE_RIGHT = False
+  ENABLE_LEFT  = False
 
   ENUM_NAME          = 0
   ENUM_TORQUE_EN_0   = 1
@@ -35,18 +35,18 @@ class Sophia:
           ("lhr", 0x32,        0x33,        0x3d,   0x32,   ENABLE_LEFT ,   0.2,            -0.2,                  1,    0.0     ),
           ("lhp", 0x34,        0x35,        0x3e,   0x34,   ENABLE_LEFT ,   0.7,            -1.3,                  2,    5.0     ),
           ("lkp", 0x36,        0x37,        0x3f,   0x36,   ENABLE_LEFT ,   1.2,             0.0,                  3,    0.0     ),
-          ("lay", 0x38,        0x38,        0x38,   0x38,   ENABLE_LEFT ,   1.0,            -1.0,                  4,    0.0     ),
-          ("lar", 0x39,        0x39,        0x39,   0x39,   ENABLE_LEFT ,   1.0,            -1.0,                  5,    0.0     ),
-          ("lap", 0x3a,        0x3a,        0x3a,   0x3a,   ENABLE_LEFT ,   0.4,            -1.0,                  6,    0.0     ),
-          ("ltp", 0x3b,        0x3b,        0x3b,   0x3b,   ENABLE_LEFT ,   1.0,            -1.0,                  7,    0.0     ),
+          ("lay", 0x38,        0x38,        0x38,   0x38,   True        ,   1.0,            -1.0,                  4,    0.0     ),
+          ("lar", 0x39,        0x39,        0x39,   0x39,   True        ,   1.0,            -1.0,                  5,    0.0     ),
+          ("lap", 0x3a,        0x3a,        0x3a,   0x3a,   True        ,   0.4,            -1.0,                  6,    0.0     ),
+          ("ltp", 0x3b,        0x3b,        0x3b,   0x3b,   True        ,   1.0,            -1.0,                  7,    0.0     ),
           ("rhy", 0x10,        0x11,        0x1c,   0x10,   ENABLE_RIGHT,   1.0,            -1.0,                  8,  -19.4     ),
           ("rhr", 0x12,        0x13,        0x1d,   0x12,   ENABLE_RIGHT,   0.2,            -0.2,                  9,    0.0     ),
           ("rhp", 0x14,        0x15,        0x1e,   0x14,   ENABLE_RIGHT,   0.7,            -1.3,                 10,    5.0     ),
           ("rkp", 0x16,        0x17,        0x1f,   0x16,   ENABLE_RIGHT,   1.2,             0.0,                 11,    0.0     ),
-          ("ray", 0x18,        0x18,        0x18,   0x18,   ENABLE_RIGHT,   1.0,            -1.0,                 12,    0.0     ),
-          ("rar", 0x19,        0x19,        0x19,   0x19,   ENABLE_RIGHT,   1.0,            -1.0,                 13,    0.0     ),
-          ("rap", 0x1a,        0x1a,        0x1a,   0x1a,   ENABLE_RIGHT,   0.4,            -1.0,                 14,    0.0     ),
-          ("rtp", 0x1b,        0x1b,        0x1b,   0x1b,   ENABLE_RIGHT,   1.0,            -1.0,                 15,    0.0     )
+          ("ray", 0x18,        0x18,        0x18,   0x18,   True        ,   1.0,            -1.0,                 12,    0.0     ),
+          ("rar", 0x19,        0x19,        0x19,   0x19,   True        ,   1.0,            -1.0,                 13,    0.0     ),
+          ("rap", 0x1a,        0x1a,        0x1a,   0x1a,   True        ,   0.4,            -1.0,                 14,    0.0     ),
+          ("rtp", 0x1b,        0x1b,        0x1b,   0x1b,   True        ,   1.0,            -1.0,                 15,    0.0     )
         }
 
 
